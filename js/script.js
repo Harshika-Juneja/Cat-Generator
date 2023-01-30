5c62918076befa970c3a37aaf861a9c69fa426f4
harshika <harshikajuneja22@gmail.com>
2023-01-31 03:37:11 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function generateCat(){
+
+    var image=document.createElement('img');
+    var div =document.getElementById('flex-cat-gen');
+    image.src="https://source.unsplash.com/160x100/?cat";
+    div.appendChild(image);
+}
